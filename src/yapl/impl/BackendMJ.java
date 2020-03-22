0739c6f9d3fff9746f21bd28f286a5e55c43b6d1
Manuel Langer <37038032+manlanger@users.noreply.github.com>
2020-03-22 16:12:09 +0100

    Implemented methods for Test1

--- src/yapl/impl/BackendMJ.java
+++ src/yapl/impl/BackendMJ.java
@@ -2,12 +2,35 @@ package yapl.impl;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 import yapl.interfaces.BackendBinSM;
+import yapl.interfaces.ICode;
 import yapl.interfaces.MemoryRegion;
 
 public class BackendMJ implements BackendBinSM {
 
+	private ByteBuffer code;
+	private IntBuffer data;
+	private Integer startPC;
+	private Integer dataSize;
+	private Map<String, Integer> enterPosition;
+	private Map<String, Integer> exitPosition;
+	
+	public BackendMJ() {
+		code = ByteBuffer.allocate(100);
+		data = IntBuffer.allocate(100);
+		
+		startPC = 0;
+		dataSize = 0;
+		
+		enterPosition = new HashMap<String, Integer>();
+		exitPosition = new HashMap<String, Integer>();
+	}
 	
 	@Override
 	public int wordSize() {
@@ -29,8 +52,37 @@ public class BackendMJ implements BackendBinSM {
 
 	@Override
 	public void writeObjectFile(OutputStream outStream) throws IOException {
-		// TODO Auto-generated method stub
-
+		// magic marker
+		outStream.write((byte)0x4d);
+		outStream.write((byte)0x4a);
+		
+		// code size
+		Integer codeSize = code.position();
+		ByteBuffer bbCodeSize = ByteBuffer.allocate(4);
+		bbCodeSize.putInt(codeSize);
+		outStream.write(bbCodeSize.array());
+		
+		// data size
+		ByteBuffer bbDataSize = ByteBuffer.allocate(4);
+		bbDataSize.putInt(dataSize);
+		outStream.write(bbDataSize.array());
+		
+		// start PC
+		ByteBuffer bbStartPC = ByteBuffer.allocate(4);
+		bbStartPC.putInt(startPC);
+		outStream.write(bbStartPC.array());
+		
+		// code
+		byte[] byteCode = new byte[codeSize];
+		code.flip();
+		code.get(byteCode, 0, codeSize);
+		outStream.write(byteCode);
+		
+		// data		
+		for (int i = 0; i < data.position(); i++) {
+			byte[] bytes = ByteBuffer.allocate(4).putInt(data.get(i)).array();
+			outStream.write(bytes);
+		}		
 	}
 
 	@Override
@@ -41,8 +93,28 @@ public class BackendMJ implements BackendBinSM {
 
 	@Override
 	public int allocStringConstant(String string) {
-		// TODO Auto-generated method stub
-		return 0;
+		int position = data.position();
+		int length = string.length();
+		
+		if (length % 4 == 0) {
+			dataSize += length/4 + 1;
+		}
+		else { 
+			dataSize += (length+3)/4;
+		}
+
+		byte[] bytes = new byte[dataSize*4];
+
+		for (int i = 0; i < length; i++) 
+			bytes[i] = string.getBytes()[i];
+		
+		for (int i = length; i < dataSize*4; i++)
+			bytes[i] = (byte)0x00;
+		
+		IntBuffer temp = ByteBuffer.wrap(bytes).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
+		data.put(temp);
+		
+		return position;
 	}
 
 	@Override
@@ -65,7 +137,7 @@ public class BackendMJ implements BackendBinSM {
 
 	@Override
 	public void allocArray() {
-		// TODO Auto-generated method stub
+		// TODO Auto-gallocStringConstantenerated method stub
 
 	}
 
@@ -113,8 +185,9 @@ public class BackendMJ implements BackendBinSM {
 
 	@Override
 	public void writeString(int addr) {
-		// TODO Auto-generated method stub
-
+		code.put(ICode.SPRINT);
+		Integer i = addr;
+		code.putShort(i.shortValue());
 	}
 
 	@Override
@@ -215,14 +288,25 @@ public class BackendMJ implements BackendBinSM {
 
 	@Override
 	public void enterProc(String label, int nParams, boolean main) {
-		// TODO Auto-generated method stub
-
+		if (main) {
+			startPC = code.position();
+		}
+		
+		enterPosition.put(label, code.position());
+		
+		Integer params = nParams;
+		
+		code.put(ICode.ENTER);
+		code.put(params.byteValue());	// nparams
+		code.put(params.byteValue()); // framesize
 	}
 
 	@Override
 	public void exitProc(String label) {
-		// TODO Auto-generated method stub
+		exitPosition.put(label, code.position());
 
+		code.put(ICode.EXIT);
+		code.put(ICode.RETURN);
 	}
 
 	@Override
---
+++ src/yapl/interfaces/ICode.java
@@ -0,0 +1,9 @@
+package yapl.interfaces;
+
+public interface ICode {
+	public static final byte ZERO	= (byte)0x00;
+	public static final byte RETURN = (byte)0x2F;
+	public static final byte ENTER	= (byte)0x30;
+	public static final byte EXIT	= (byte)0x31;
+	public static final byte SPRINT = (byte)0x37;
+}
