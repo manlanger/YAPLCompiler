5121ddb34d29be322f606e166b3f63d123b95281
Alexander Isopp <alisopp@edu.aau.at>
2020-03-25 21:21:24 +0100

    finished test 4

--- src/yapl/impl/BackendMJ.java
+++ src/yapl/impl/BackendMJ.java
@@ -15,377 +15,410 @@ import yapl.interfaces.MemoryRegion;
 
 public class BackendMJ implements BackendBinSM {
 
-	private ByteBuffer code;
-	private IntBuffer data;
-	private Integer startPC;
-	private Integer dataSize;
-	private Map<String, Short> labelPosition;
-	private Map<String, ArrayList<Integer>> backPatching;
-	
-	public BackendMJ() {
-		code = ByteBuffer.allocate(100);
-		data = IntBuffer.allocate(100);
-		
-		startPC = 0;
-		dataSize = 0;
-		
-		labelPosition = new HashMap<String, Short>();
-		backPatching = new HashMap<String, ArrayList<Integer>>();
-	}
-	
-	@Override
-	public int wordSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    private ByteBuffer code;
+    private IntBuffer data;
+    private Integer startPC;
+    private Integer dataSize;
+    private Map<String, Short> labelPosition;
+    private Map<String, ArrayList<Integer>> backPatching;
 
-	@Override
-	public int boolValue(boolean value) {
-		return value ? 1 : 0;
-	}
+    public BackendMJ() {
+        code = ByteBuffer.allocate(100);
+        data = IntBuffer.allocate(100);
 
-	@Override
-	public void assignLabel(String label) {
-		manageLabelPosition(label);
-	}
+        startPC = 0;
+        dataSize = 0;
 
-	@Override
-	public void writeObjectFile(OutputStream outStream) throws IOException {
-		// magic marker
-		outStream.write((byte)0x4d);
-		outStream.write((byte)0x4a);
-		
-		// code size
-		Integer codeSize = code.position();
-		ByteBuffer bbCodeSize = ByteBuffer.allocate(4);
-		bbCodeSize.putInt(codeSize);
-		outStream.write(bbCodeSize.array());
-		
-		// data size
-		ByteBuffer bbDataSize = ByteBuffer.allocate(4);
-		bbDataSize.putInt(dataSize);
-		outStream.write(bbDataSize.array());
-		
-		// start PC
-		ByteBuffer bbStartPC = ByteBuffer.allocate(4);
-		bbStartPC.putInt(startPC);
-		outStream.write(bbStartPC.array());
-		
-		// code
-		byte[] byteCode = new byte[codeSize];
-		code.flip();
-		code.get(byteCode, 0, codeSize);
-		outStream.write(byteCode);
-		
-		// data		
-		for (int i = 0; i < data.position(); i++) {
-			byte[] bytes = ByteBuffer.allocate(4).putInt(data.get(i)).array();
-			outStream.write(bytes);
-		}		
-	}
+        labelPosition = new HashMap<String, Short>();
+        backPatching = new HashMap<String, ArrayList<Integer>>();
+    }
 
-	@Override
-	public int allocStaticData(int words) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int wordSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public int allocStringConstant(String string) {
-		int position = data.position();
-		int length = string.length();
-		int stringSize;
-		
-		if (length % 4 == 0) {
-			stringSize = length/4 + 1;
-		}
-		else { 
-			stringSize = (length+3)/4;
-		}
-		
-		dataSize += stringSize;
+    @Override
+    public int boolValue(boolean value) {
+        return value ? 1 : 0;
+    }
 
-		byte[] bytes = new byte[stringSize*4];
+    @Override
+    public void assignLabel(String label) {
+        manageLabelPosition(label);
+    }
 
-		for (int i = 0; i < length; i++) 
-			bytes[i] = string.getBytes()[i];
-		
-		for (int i = length; i < stringSize*4; i++)
-			bytes[i] = (byte)0x00;
-		
-		IntBuffer temp = ByteBuffer.wrap(bytes).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
-		data.put(temp);
-		
-		return position;
-	}
+    @Override
+    public void writeObjectFile(OutputStream outStream) throws IOException {
+        // magic marker
+        outStream.write((byte) 0x4d);
+        outStream.write((byte) 0x4a);
 
-	@Override
-	public int allocStack(int words) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+        // code size
+        Integer codeSize = code.position();
+        ByteBuffer bbCodeSize = ByteBuffer.allocate(4);
+        bbCodeSize.putInt(codeSize);
+        outStream.write(bbCodeSize.array());
 
-	@Override
-	public void allocHeap(int words) {
-		// TODO Auto-generated method stub
+        // data size
+        ByteBuffer bbDataSize = ByteBuffer.allocate(4);
+        bbDataSize.putInt(dataSize);
+        outStream.write(bbDataSize.array());
 
-	}
+        // start PC
+        ByteBuffer bbStartPC = ByteBuffer.allocate(4);
+        bbStartPC.putInt(startPC);
+        outStream.write(bbStartPC.array());
 
-	@Override
-	public void storeArrayDim(int dim) {
-		// TODO Auto-generated method stub
+        // code
+        byte[] byteCode = new byte[codeSize];
+        code.flip();
+        code.get(byteCode, 0, codeSize);
+        outStream.write(byteCode);
 
-	}
+        // data
+        for (int i = 0; i < data.position(); i++) {
+            byte[] bytes = ByteBuffer.allocate(4).putInt(data.get(i)).array();
+            outStream.write(bytes);
+        }
+    }
 
-	@Override
-	public void allocArray() {
-		// TODO Auto-gallocStringConstantenerated method stub
+    @Override
+    public int allocStaticData(int words) {
+        int position = data.position();
+        int length = 4 * words;
+        byte[] bytes = new byte[length];
+        dataSize += words;
+        for (int i = 0; i < length; i++) {
+            bytes[i] = (byte) 0x00;
+        }
 
-	}
+        IntBuffer temp = ByteBuffer.wrap(bytes).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
+        data.put(temp);
+        return position;
+    }
 
-	@Override
-	public void loadConst(int value) {
-		code.put(ICode.CONST);
-		
-		Integer val = value;
-		
-		code.putInt(val);
-	}
+    @Override
+    public int allocStringConstant(String string) {
+        int position = data.position();
+        int length = string.length();
+        int stringSize;
 
-	@Override
-	public void loadWord(MemoryRegion region, int offset) {
-		if (region == MemoryRegion.STACK) {
-			code.put(ICode.LOAD);
-			
-			Integer off = offset;
-			code.put(off.byteValue());
-		}
-	}
+        if (length % 4 == 0) {
+            stringSize = length / 4 + 1;
+        } else {
+            stringSize = (length + 3) / 4;
+        }
 
-	@Override
-	public void storeWord(MemoryRegion region, int offset) {
-		if (region == MemoryRegion.STACK) {
-			code.put(ICode.STORE);
-			
-			Integer off = offset;
-			code.put(off.byteValue());
-		}
-	}
+        dataSize += stringSize;
 
-	@Override
-	public void loadArrayElement() {
-		// TODO Auto-generated method stub
+        byte[] bytes = new byte[stringSize * 4];
 
-	}
+        for (int i = 0; i < length; i++)
+            bytes[i] = string.getBytes()[i];
 
-	@Override
-	public void storeArrayElement() {
-		// TODO Auto-generated method stub
+        for (int i = length; i < stringSize * 4; i++)
+            bytes[i] = (byte) 0x00;
 
-	}
+        IntBuffer temp = ByteBuffer.wrap(bytes).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
+        data.put(temp);
 
-	@Override
-	public void arrayLength() {
-		// TODO Auto-generated method stub
+        return position;
+    }
 
-	}
+    @Override
+    public int allocStack(int words) {
+        int position;
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public void writeInteger() {
-		code.put(ICode.CONST0);
-		code.put(ICode.PRINT);
-	}
+    @Override
+    public void allocHeap(int words) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void writeString(int addr) {
-		code.put(ICode.SPRINT);
-		Integer i = addr;
-		code.putShort(i.shortValue());
-	}
+    }
 
-	@Override
-	public void neg() {
-		// TODO Auto-generated method stub
+    @Override
+    public void storeArrayDim(int dim) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void add() {
-		// TODO Auto-generated method stub
+    @Override
+    public void allocArray() {
+        // TODO Auto-gallocStringConstantenerated method stub
 
-	}
+    }
 
-	@Override
-	public void sub() {
-		// TODO Auto-generated method stub
+    @Override
+    public void loadConst(int value) {
+        code.put(ICode.CONST);
 
-	}
+        Integer val = value;
 
-	@Override
-	public void mul() {
-		// TODO Auto-generated method stub
+        code.putInt(val);
+    }
 
-	}
+    @Override
+    public void loadWord(MemoryRegion region, int offset) {
+        if (region == MemoryRegion.STACK) {
+            code.put(ICode.LOAD);
 
-	@Override
-	public void div() {
-		// TODO Auto-generated method stub
+            Integer off = offset;
+            code.put(off.byteValue());
+        } else if (region == MemoryRegion.STATIC) {
+            code.put(ICode.GET_STATIC);
 
-	}
+            Integer off = offset;
+            code.putShort(off.shortValue());
+            /*byte[] bytes = ByteBuffer.allocate(2).putShort(off.shortValue()).order(ByteOrder.BIG_ENDIAN).array();
+            for (byte cByte : bytes) {
+                code.put(cByte);
+            }*/
+            //TODO Static Region
+        }
+        //TODO Heap Region
+    }
 
-	@Override
-	public void mod() {
-		// TODO Auto-generated method stub
+    @Override
+    public void storeWord(MemoryRegion region, int offset) {
+        if (region == MemoryRegion.STACK) {
+            code.put(ICode.STORE);
 
-	}
+            Integer off = offset;
+            code.put(off.byteValue());
+        } else if (region == MemoryRegion.STATIC) {
+            code.put(ICode.PUT_STATIC);
+            //TODO Static Region
+            Integer off = offset;
+            code.putShort(off.shortValue());
+            /*byte[] bytes = ByteBuffer.allocate(2).putShort(off.shortValue()).order(ByteOrder.BIG_ENDIAN).array();
+            for (byte cByte : bytes) {
+                code.put(cByte);
+            }*/
+        }
 
-	@Override
-	public void and() {
-		// TODO Auto-generated method stub
+        //TODO Heap Region
+    }
 
-	}
+    @Override
+    public void loadArrayElement() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void or() {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void storeArrayElement() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void isEqual() {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void arrayLength() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void isLess() {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void writeInteger() {
+        code.put(ICode.CONST0);
+        code.put(ICode.PRINT);
+    }
 
-	@Override
-	public void isLessOrEqual() {
-		// TODO Auto-generated method stub
+    @Override
+    public void writeString(int addr) {
+        code.put(ICode.SPRINT);
+        Integer i = addr;
+        code.putShort(i.shortValue());
+    }
 
-	}
+    @Override
+    public void neg() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void isGreater() {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void add() {
+        code.put(ICode.ADD);
+        // TODO Auto-generated method stub
 
-	@Override
-	public void isGreaterOrEqual() {
-		// TODO Auto-generated method stub
+    }
 
-	}
-	
-	/*
-	 * Checks if label is known already. 
-	 * If address of label is known, puts address into code buffer, 
-	 * otherwise adds label to backpatching list
-	 * @param label 	The label for which the address needs to be determined
-	 */
-	private void putAddress(String label) {
-		if (labelPosition.containsKey(label)) {
-			short pos = labelPosition.get(label);
-			code.putShort(pos);
-		} else {
-			addBackPatching(label, code.position());
-		}
-	}
+    @Override
+    public void sub() {
+        code.put(ICode.SUB);
 
-	@Override
-	public void branchIf(boolean value, String label) {
-		int intVal = value ? 1 : 0;
-		loadConst(intVal);
-		
-		code.put(ICode.JEQ);
+    }
 
-		putAddress(label);
-	}
-	
-	private void addBackPatching(String label, int pos) {
-		ArrayList<Integer> positions;
-		
-		if (backPatching.containsKey(label)) {
-			positions = backPatching.get(label);
-		} else {
-			positions = new ArrayList<Integer>();
-		}
-		
-		positions.add(pos);
-		backPatching.put(label, positions);
-		
-		code.putShort(ICode.TEMP);
-	}
+    @Override
+    public void mul() {
+        code.put(ICode.MUL);
+        // TODO Auto-generated method stub
 
-	@Override
-	public void jump(String label) {
-		code.put(ICode.JMP);
-		
-		putAddress(label);
-	}
+    }
 
-	@Override
-	public void callProc(String label) {
-		code.put(ICode.CALL);
-		
-		putAddress(label);
-	}
-	
-	/*
-	 * Checks if this label is in the back patching list.
-	 * If true, performs back patching and removes label from back patching list.
-	 * @param label 	The label for which the back patching needs to be checked
-	 */
-	private void manageLabelPosition(String label) {
-		labelPosition.put(label, (short)code.position());
-		
-		if (backPatching.containsKey(label)) {
-			
-			ArrayList<Integer> positions = backPatching.get(label);
-			
-			int position = code.position();
-			
-			for (Integer pos : positions) {
-				code.position(pos);
-				code.putShort((short)position);
-			}
-			
-			code.position(position);
-			backPatching.remove(label);
-		}
-	}
+    @Override
+    public void div() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void enterProc(String label, int nParams, boolean main) {
-		if (main) {
-			startPC = code.position();
-		}
-		
-		Integer params = nParams;
+    }
 
-		manageLabelPosition(label);
-		
-		code.put(ICode.ENTER);
-		code.put(params.byteValue());	// nparams
-		code.put(params.byteValue()); // framesize
-	}
+    @Override
+    public void mod() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void exitProc(String label) {
-		manageLabelPosition(label);
+    }
 
-		code.put(ICode.EXIT);
-		code.put(ICode.RETURN);
-	}
+    @Override
+    public void and() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public int paramOffset(int index) {
-		// TODO implement correctly
-		return index;
-	}
+    }
+
+    @Override
+    public void or() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void isEqual() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void isLess() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void isLessOrEqual() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void isGreater() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void isGreaterOrEqual() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * Checks if label is known already.
+     * If address of label is known, puts address into code buffer,
+     * otherwise adds label to backpatching list
+     * @param label 	The label for which the address needs to be determined
+     */
+    private void putAddress(String label) {
+        if (labelPosition.containsKey(label)) {
+            short pos = labelPosition.get(label);
+            code.putShort(pos);
+        } else {
+            addBackPatching(label, code.position());
+        }
+    }
+
+    @Override
+    public void branchIf(boolean value, String label) {
+        int intVal = value ? 1 : 0;
+        loadConst(intVal);
+
+        code.put(ICode.JEQ);
+
+        putAddress(label);
+    }
+
+    private void addBackPatching(String label, int pos) {
+        ArrayList<Integer> positions;
+
+        if (backPatching.containsKey(label)) {
+            positions = backPatching.get(label);
+        } else {
+            positions = new ArrayList<Integer>();
+        }
+
+        positions.add(pos);
+        backPatching.put(label, positions);
+
+        code.putShort(ICode.TEMP);
+    }
+
+    @Override
+    public void jump(String label) {
+        code.put(ICode.JMP);
+
+        putAddress(label);
+    }
+
+    @Override
+    public void callProc(String label) {
+        code.put(ICode.CALL);
+
+        putAddress(label);
+    }
+
+    /*
+     * Checks if this label is in the back patching list.
+     * If true, performs back patching and removes label from back patching list.
+     * @param label 	The label for which the back patching needs to be checked
+     */
+    private void manageLabelPosition(String label) {
+        labelPosition.put(label, (short) code.position());
+
+        if (backPatching.containsKey(label)) {
+
+            ArrayList<Integer> positions = backPatching.get(label);
+
+            int position = code.position();
+
+            for (Integer pos : positions) {
+                code.position(pos);
+                code.putShort((short) position);
+            }
+
+            code.position(position);
+            backPatching.remove(label);
+        }
+    }
+
+    @Override
+    public void enterProc(String label, int nParams, boolean main) {
+        if (main) {
+            startPC = code.position();
+        }
+
+        Integer params = nParams;
+
+        manageLabelPosition(label);
+
+        code.put(ICode.ENTER);
+        code.put(params.byteValue());    // nparams
+        code.put(params.byteValue()); // framesize
+    }
+
+    @Override
+    public void exitProc(String label) {
+        manageLabelPosition(label);
+
+        code.put(ICode.EXIT);
+        code.put(ICode.RETURN);
+    }
+
+    @Override
+    public int paramOffset(int index) {
+        // TODO implement correctly
+        return index;
+    }
 
 }
--- src/yapl/interfaces/ICode.java
+++ src/yapl/interfaces/ICode.java
@@ -1,25 +1,31 @@
 package yapl.interfaces;
 
 public interface ICode {
-	/*
-	 * byte constants
-	 */
-	public static final byte ZERO	= (byte)0x00;
-	public static final byte LOAD	= (byte)0x01;
-	public static final byte STORE	= (byte)0x06;
-	public static final byte CONST0	= (byte)0x0F;
-	public static final byte CONST	= (byte)0x16;
-	public static final byte JMP	= (byte)0x27;
-	public static final byte JEQ	= (byte)0x28;
-	public static final byte CALL	= (byte)0x2E;
-	public static final byte RETURN = (byte)0x2F;
-	public static final byte ENTER	= (byte)0x30;
-	public static final byte EXIT	= (byte)0x31;
-	public static final byte PRINT	= (byte)0x33;
-	public static final byte SPRINT = (byte)0x37;
-	
-	/*
-	 * short constants
-	 */
-	public static final short TEMP	= (byte)0xFFFF;
+    /*
+     * byte constants
+     */
+    public static final byte ZERO = (byte) 0x00;
+    public static final byte LOAD = (byte) 0x01;
+    public static final byte STORE = (byte) 0x06;
+    public static final byte CONST0 = (byte) 0x0F;
+    public static final byte CONST = (byte) 0x16;
+    public static final byte JMP = (byte) 0x27;
+    public static final byte JEQ = (byte) 0x28;
+    public static final byte CALL = (byte) 0x2E;
+    public static final byte RETURN = (byte) 0x2F;
+    public static final byte ENTER = (byte) 0x30;
+    public static final byte EXIT = (byte) 0x31;
+    public static final byte PRINT = (byte) 0x33;
+    public static final byte SPRINT = (byte) 0x37;
+    public static final byte GET_STATIC = (byte) 0x0B;
+    public static final byte PUT_STATIC = (byte) 0x0C;
+
+    public static final byte ADD = (byte) 0x17;
+    public static final byte SUB = (byte) 0x18;
+    public static final byte MUL = (byte) 0x19;
+    public static final byte LOAD_ZERO = (byte)0x02;
+    /*
+     * short constants
+     */
+    public static final short TEMP = (byte) 0xFFFF;
 }
